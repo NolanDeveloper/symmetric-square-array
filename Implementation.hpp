dea9ac703ab05bc2b4770412943fc1e481eb1caf
Nolan <nolane16@gmail.com>
2017-03-10 21:39:06 +0300

    Added copy on write semantics

---
+++ Implementation.hpp
@@ -0,0 +1,349 @@
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+
+namespace metaprogramming {
+
+template <typename ValueType, typename Allocator>
+class SymmetricSquareArray;
+
+namespace ssa {
+
+template <typename ValueType, typename Allocator>
+class Implementation {
+    size_t rank;
+    size_t size;
+    size_t capacity;
+    Allocator allocator;
+    ValueType * data;
+
+    // Returns sum of first n natural numbers
+    static size_t sum_n(size_t n) {
+        return (n + 1) * n / 2;
+    }
+
+    static size_t calculate_size(size_t rank) {
+        return sum_n(rank);
+    }
+
+    static size_t at(size_t row, size_t col) {
+        return col > row ? at(col, row) : sum_n(row) + col;
+    }
+
+    static void check_arguments(bool condition) {
+        if (condition) return;
+        throw std::runtime_error("Function was called with bad arguments");
+    }
+
+    friend class SymmetricSquareArray<ValueType, Allocator>;
+
+public:
+    Implementation(Allocator allocator = Allocator())
+            : rank(0)
+            , size(0)
+            , capacity(0)
+            , allocator(allocator)
+            , data(nullptr) { }
+
+    Implementation(size_t rank, Allocator allocator = Allocator())
+            : rank(rank)
+            , size(calculate_size(rank))
+            , capacity(size)
+            , allocator(allocator)
+            , data(allocator.allocate(size)) {
+        for (size_t i = 0; i < size; ++i) new (&data[i]) ValueType();
+    }
+
+    Implementation(const Implementation & o)
+            : rank(o.rank)
+            , size(o.size)
+            , capacity(o.size)
+            , allocator(o.allocator)
+            , data(allocator.allocate(size)) {
+        std::uninitialized_copy(o.data, o.data + o.size, data);
+    }
+
+    Implementation(Implementation && o)
+            : Implementation() {
+        swap(*this, o);
+    }
+
+    ~Implementation() {
+        for (size_t i = 0; i < size; ++i) data[i].~ValueType();
+        allocator.deallocate(data, capacity);
+    }
+
+    void insert(size_t row, size_t col,
+                const ValueType & val,
+                const ValueType & nil = ValueType()) {
+        if (col != row) {
+            if (row < col) std::swap(row, col);
+            insert(col, col, nil, nil);
+            insert(row, row, nil, nil);
+            (*this)(row, col) = val;
+        } else {
+            check_arguments(row <= rank && col <= rank);
+            size_t new_rank = rank + 1;
+            size_t new_size = size + new_rank;
+            if (new_size <= capacity) {
+                ValueType * it = &data[new_rank * (new_rank + 1) / 2 - 1];
+                for (size_t r = new_rank - 1; r < new_rank; --r) {
+                    for (size_t c = r; c <= r; --c) {
+                        if (r < row) return;
+                        if (r == row) {
+                            *it = c < row ? nil : std::move(val);
+                        } else if (r + 1 < new_rank) {
+                            if (c < row) {
+                                *it = std::move(data[at(r - 1, c)]);
+                            } else if (c == row) {
+                                *it = nil;
+                            } else {
+                                *it = std::move(data[at(r - 1, c - 1)]);
+                            }
+                        } else {
+                            if (c < row) {
+                                new (it) ValueType(std::move(
+                                    data[at(r - 1, c)]));
+                            } else if (c == row) {
+                                new (it) ValueType(nil);
+                            } else {
+                                new (it) ValueType(std::move(
+                                    data[at(r - 1, c - 1)]));
+                            }
+                        }
+                        --it;
+                    }
+                }
+            } else {
+                ValueType * new_data = allocator.allocate(new_size);
+                ValueType * it = &new_data[new_rank * (new_rank + 1) / 2 - 1];
+                for (size_t r = new_rank - 1; r < new_rank; --r) {
+                    for (size_t c = r; c <= r; --c) {
+                        if (r < row) {
+                            new (it) ValueType(std::move(data[at(r, c)]));
+                        } else if (r == row) {
+                            new (it) ValueType(c < row ? nil : std::move(val));
+                        } else {
+                            if (c < row) {
+                                new (it) ValueType(std::move(
+                                    data[at(r - 1, c)]));
+                            } else if (c == row) {
+                                new (it) ValueType(nil);
+                            } else {
+                                new (it) ValueType(std::move(
+                                    data[at(r - 1, c - 1)]));
+                            }
+                        }
+                        --it;
+                    }
+                }
+                for (size_t i = 0; i < size; ++i) data[i].~ValueType();
+                allocator.deallocate(data, size);
+                data = new_data;
+                capacity = new_size;
+            }
+            size = new_size;
+            rank = new_rank;
+        }
+    }
+
+    void erase(size_t row, size_t col) {
+        if (row != col) {
+            if (row < col) std::swap(row, col);
+            erase(row, row);
+            erase(col, col);
+        } else {
+            check_arguments(row <= rank && col <= rank);
+            size_t new_rank = rank - 1;
+            size_t new_size = size - rank;
+            for (size_t r = 0; r < new_rank; ++r) {
+                if (r < row) continue;
+                for (size_t c = 0; c <= r; ++c) {
+                    data[at(r, c)] = c < row
+                        ? std::move(data[at(r + 1, c)])
+                        : std::move(data[at(r + 1, c + 1)]);
+                }
+            }
+            size_t start = at(rank - 1, 0);
+            size_t end = at(rank - 1, rank - 1) + 1;
+            for (size_t i = start; i < end; ++i) data[i].~ValueType();
+            rank = new_rank;
+            size = new_size;
+        }
+    }
+
+    size_t get_rank() const { return rank; }
+
+    Implementation & operator=(Implementation o) {
+        swap(*this, o);
+        return *this;
+    }
+
+    ValueType & operator()(size_t row, size_t col) {
+        return data[at(row, col)];
+    }
+
+    const ValueType & operator()(size_t row, size_t col) const {
+        return data[at(row, col)];
+    }
+
+    static void swap(Implementation & lhs, Implementation & rhs) {
+        std::swap(lhs.rank, rhs.rank);
+        std::swap(lhs.size, rhs.size);
+        std::swap(lhs.capacity, rhs.capacity);
+        std::swap(lhs.data, rhs.data);
+        std::swap(lhs.allocator, rhs.allocator);
+    }
+
+private:
+    template <bool IsConst>
+    class ImplementationIterator {
+    public:
+        using difference_type = ptrdiff_t;
+        using value_type = std::conditional_t<IsConst,
+                                              const ValueType,
+                                              ValueType>;
+        using pointer = ValueType *;
+        using reference = ValueType &;
+        using iterator_category = std::random_access_iterator_tag;
+
+    private:
+        using arrayptr_t =
+            std::conditional_t<
+                IsConst,
+                const Implementation *,
+                Implementation *>;
+        arrayptr_t array;
+        size_t row;
+        size_t col;
+
+        size_t to_linear() { return row * array->rank + col; }
+
+    public:
+        ImplementationIterator()
+                : array(nullptr)
+                , row(0)
+                , col(0) { }
+
+        ImplementationIterator(
+                    arrayptr_t array,
+                    size_t row,
+                    size_t col)
+                : array(array)
+                , row(row)
+                , col(col) { }
+
+        ImplementationIterator & operator++() {
+            ++col;
+            if (array->rank <= col) {
+                col = 0;
+                ++row;
+            }
+            return *this;
+        }
+
+        ImplementationIterator operator++(int) {
+            ImplementationIterator it(*this);
+            ++(*this);
+            return it;
+        }
+
+        ImplementationIterator & operator--() {
+            --col;
+            if (array->rank <= col) {
+                col = array->rank - 1;
+                --row;
+            }
+            return *this;
+        }
+
+        ImplementationIterator operator--(int) {
+            ImplementationIterator it(*this);
+            --(*this);
+            return it;
+        }
+
+        ValueType & operator*() { return (*array)(row, col); }
+        ValueType * operator->() { return std::addressof((*array)[row, col]); }
+
+        ImplementationIterator & operator+=(difference_type x) {
+            auto rank = array->rank;
+            difference_type t = to_linear() + x;
+            row = t / rank;
+            col = t % rank;
+            return *this;
+        }
+
+        ImplementationIterator & operator-=(difference_type x) {
+            auto rank = array->rank;
+            difference_type t = to_linear() - x;
+            row = t / rank;
+            col = t / rank;
+            return *this;
+        }
+
+        ValueType & operator[](difference_type x) { return *(*this + x); }
+
+        friend ImplementationIterator operator+(
+                ImplementationIterator lhs,
+                difference_type rhs) { return lhs += rhs; }
+
+        friend ImplementationIterator operator-(
+                const ImplementationIterator & lhs,
+                difference_type rhs) { return lhs -= rhs; }
+
+        friend bool operator==(const ImplementationIterator & lhs,
+                               const ImplementationIterator & rhs) {
+            return lhs.row == rhs.row && lhs.col == rhs.col;
+        }
+
+        friend bool operator!=(const ImplementationIterator & lhs,
+                               const ImplementationIterator & rhs) {
+            return !(lhs == rhs);
+        }
+
+        friend bool operator<(const ImplementationIterator & lhs,
+                              const ImplementationIterator & rhs) {
+            return lhs.to_linear() < rhs.to_linear();
+        }
+
+        friend bool operator<=(const ImplementationIterator & lhs,
+                              const ImplementationIterator & rhs) {
+            return lhs.to_linear() <= rhs.to_linear();
+        }
+
+        friend bool operator>(const ImplementationIterator & lhs,
+                              const ImplementationIterator & rhs) {
+            return !(lhs.to_linear() <= rhs.to_linear());
+        }
+
+        friend bool operator>=(const ImplementationIterator & lhs,
+                              const ImplementationIterator & rhs) {
+            return !(lhs.to_linear() < rhs.to_linear());
+        }
+    };
+
+public:
+    using Iterator      = ImplementationIterator<false>;
+    using ConstIterator = ImplementationIterator<true>;
+
+    Iterator begin() { return { this, 0, 0 }; }
+    Iterator end()   { return { this, rank, 0 }; }
+
+    ConstIterator begin() const { return { this, 0, 0 }; }
+    ConstIterator end()   const { return { this, rank, 0 }; }
+
+    ConstIterator cbegin() const { return { this, 0, 0 }; }
+    ConstIterator cend()   const { return { this, rank, 0 }; }
+
+};
+
+}
+
+}
+
--- SymmetricSquareArray.hpp
+++ SymmetricSquareArray.hpp
@@ -1,341 +1,131 @@
 #pragma once
 
-#include <cassert>
-#include <cstddef>
+#include "Implementation.hpp"
 
-#include <iterator>
 #include <memory>
-#include <stdexcept>
 
 namespace metaprogramming {
 
-template <typename ValueType,
-          typename Allocator = std::allocator<ValueType>>
-class SymmetricSquareArray {
-    size_t rank;
-    size_t size;
-    size_t capacity;
-    Allocator allocator;
-    ValueType * data;
+namespace ssa {
 
-    // Returns sum of first n natural numbers
-    static size_t sum_n(size_t n) {
-        return (n + 1) * n / 2;
-    }
+template <typename ValueType, typename Allocator>
+struct ImplementationHolder {
+    using ImplementationType = Implementation<ValueType, Allocator>;
 
-    static size_t calculate_size(size_t rank) {
-        return sum_n(rank);
-    }
+    bool is_sharable;
+    ImplementationType implementation;
 
-    static size_t at(size_t row, size_t col) {
-        return col > row ? at(col, row) : sum_n(row) + col;
-    }
+    ImplementationHolder(Allocator allocator = Allocator())
+        : is_sharable(true)
+        , implementation(allocator) { }
 
-    static void check_arguments(bool condition) {
-        if (condition) return;
-        throw std::runtime_error("Function was called with bad arguments");
-    }
+    ImplementationHolder(size_t rank, Allocator allocator = Allocator())
+        : is_sharable(true)
+        , implementation(rank, allocator) { }
 
-public:
-    SymmetricSquareArray(Allocator allocator = Allocator())
-            : rank(0)
-            , size(0)
-            , capacity(0)
-            , allocator(allocator)
-            , data(nullptr) { }
-
-    SymmetricSquareArray(size_t rank, Allocator allocator = Allocator())
-            : rank(rank)
-            , size(calculate_size(rank))
-            , capacity(size)
-            , allocator(allocator)
-            , data(allocator.allocate(size)) {
-        for (size_t i = 0; i < size; ++i) new (&data[i]) ValueType();
-    }
-
-    SymmetricSquareArray(const SymmetricSquareArray & o)
-            : rank(o.rank)
-            , size(o.size)
-            , capacity(o.size)
-            , allocator(o.allocator)
-            , data(allocator.allocate(size)) {
-        std::uninitialized_copy(o.data, o.data + o.size, data);
-    }
-
-    SymmetricSquareArray(SymmetricSquareArray && o)
-            : SymmetricSquareArray() {
-        swap(*this, o);
-    }
-
-    ~SymmetricSquareArray() {
-        for (size_t i = 0; i < size; ++i) data[i].~ValueType();
-        allocator.deallocate(data, capacity);
-    }
-
-    void insert(size_t row, size_t col,
-                const ValueType & val,
-                const ValueType & nil = ValueType()) {
-        if (col == row) {
-            check_arguments(row <= rank && col <= rank);
-            size_t new_rank = rank + 1;
-            size_t new_size = size + new_rank;
-            if (new_size <= capacity) {
-                ValueType * it = &data[new_rank * (new_rank + 1) / 2 - 1];
-                for (size_t r = new_rank - 1; r < new_rank; --r) {
-                    for (size_t c = r; c <= r; --c) {
-                        if (r < row) return;
-                        if (r == row) {
-                            *it = c < row ? nil : std::move(val);
-                        } else if (r + 1 < new_rank) {
-                            if (c < row) {
-                                *it = std::move(data[at(r - 1, c)]);
-                            } else if (c == row) {
-                                *it = nil;
-                            } else {
-                                *it = std::move(data[at(r - 1, c - 1)]);
-                            }
-                        } else {
-                            if (c < row) {
-                                new (it) ValueType(std::move(
-                                    data[at(r - 1, c)]));
-                            } else if (c == row) {
-                                new (it) ValueType(nil);
-                            } else {
-                                new (it) ValueType(std::move(
-                                    data[at(r - 1, c - 1)]));
-                            }
-                        }
-                        --it;
-                    }
-                }
-            } else {
-                ValueType * new_data = allocator.allocate(new_size);
-                ValueType * it = &new_data[new_rank * (new_rank + 1) / 2 - 1];
-                for (size_t r = new_rank - 1; r < new_rank; --r) {
-                    for (size_t c = r; c <= r; --c) {
-                        if (r < row) {
-                            new (it) ValueType(std::move(data[at(r, c)]));
-                        } else if (r == row) {
-                            new (it) ValueType(c < row ? nil : std::move(val));
-                        } else {
-                            if (c < row) {
-                                new (it) ValueType(std::move(
-                                    data[at(r - 1, c)]));
-                            } else if (c == row) {
-                                new (it) ValueType(nil);
-                            } else {
-                                new (it) ValueType(std::move(
-                                    data[at(r - 1, c - 1)]));
-                            }
-                        }
-                        --it;
-                    }
-                }
-                for (size_t i = 0; i < size; ++i) data[i].~ValueType();
-                allocator.deallocate(data, size);
-                data = new_data;
-                capacity = new_size;
-            }
-            size = new_size;
-            rank = new_rank;
-        } else {
-            if (row < col) std::swap(row, col);
-            insert(col, col, nil, nil);
-            insert(row, row, nil, nil);
-            (*this)(row, col) = val;
-        }
-    }
-
-    void erase(size_t row, size_t col) {
-        if (row == col) {
-            check_arguments(row <= rank && col <= rank);
-            size_t new_rank = rank - 1;
-            size_t new_size = size - rank;
-            for (size_t r = 0; r < new_rank; ++r) {
-                if (r < row) continue;
-                for (size_t c = 0; c <= r; ++c) {
-                    data[at(r, c)] = c < row
-                        ? std::move(data[at(r + 1, c)])
-                        : std::move(data[at(r + 1, c + 1)]);
-                }
-            }
-            size_t start = at(rank - 1, 0);
-            size_t end = at(rank - 1, rank - 1) + 1;
-            for (size_t i = start; i < end; ++i) data[i].~ValueType();
-            rank = new_rank;
-            size = new_size;
-        } else {
-            if (row < col) std::swap(row, col);
-            erase(row, row);
-            erase(col, col);
-        }
-    }
-
-    size_t get_rank() const { return rank; }
-
-    SymmetricSquareArray & operator=(SymmetricSquareArray o) {
-        swap(*this, o);
-        return * this;
-    }
-
-    ValueType & operator()(size_t row, size_t col) {
-        return data[at(row, col)];
-    }
-
-    const ValueType & operator()(size_t row, size_t col) const {
-        return data[at(row, col)];
-    }
-
-    static void swap(SymmetricSquareArray & lhs, SymmetricSquareArray & rhs) {
-        std::swap(lhs.rank, rhs.rank);
-        std::swap(lhs.size, rhs.size);
-        std::swap(lhs.capacity, rhs.capacity);
-        std::swap(lhs.data, rhs.data);
-        std::swap(lhs.allocator, rhs.allocator);
-    }
-
-private:
-    template <bool IsConst>
-    class SymmetricSquareArrayIterator {
-    public:
-        using difference_type = ptrdiff_t;
-        using value_type = std::conditional_t<IsConst,
-                                              const ValueType,
-                                              ValueType>;
-        using pointer = ValueType *;
-        using reference = ValueType &;
-        using iterator_category = std::random_access_iterator_tag;
-
-    private:
-        using arrayptr_t =
-            std::conditional_t<
-                IsConst,
-                const SymmetricSquareArray *,
-                SymmetricSquareArray *>;
-        arrayptr_t array;
-        size_t row;
-        size_t col;
-
-        size_t to_linear() { return row * array->rank + col; }
-
-    public:
-        SymmetricSquareArrayIterator()
-                : array(nullptr)
-                , row(0)
-                , col(0) { }
-
-        SymmetricSquareArrayIterator(
-                    arrayptr_t array,
-                    size_t row,
-                    size_t col)
-                : array(array)
-                , row(row)
-                , col(col) { }
-
-        SymmetricSquareArrayIterator & operator++() {
-            ++col;
-            if (array->rank <= col) {
-                col = 0;
-                ++row;
-            }
-            return *this;
-        }
-
-        SymmetricSquareArrayIterator operator++(int) {
-            SymmetricSquareArrayIterator it(*this);
-            ++(*this);
-            return it;
-        }
-
-        SymmetricSquareArrayIterator & operator--() {
-            --col;
-            if (array->rank <= col) {
-                col = array->rank - 1;
-                --row;
-            }
-            return *this;
-        }
-
-        SymmetricSquareArrayIterator operator--(int) {
-            SymmetricSquareArrayIterator it(*this);
-            --(*this);
-            return it;
-        }
-
-        ValueType & operator*() { return (*array)(row, col); }
-        ValueType * operator->() { return std::addressof((*array)[row, col]); }
-
-        SymmetricSquareArrayIterator & operator+=(difference_type x) {
-            auto rank = array->rank;
-            difference_type t = to_linear() + x;
-            row = t / rank;
-            col = t % rank;
-            return *this;
-        }
-
-        SymmetricSquareArrayIterator & operator-=(difference_type x) {
-            auto rank = array->rank;
-            difference_type t = to_linear() - x;
-            row = t / rank;
-            col = t / rank;
-            return *this;
-        }
-
-        ValueType & operator[](difference_type x) { return *(*this + x); }
-
-        friend SymmetricSquareArrayIterator operator+(
-                SymmetricSquareArrayIterator lhs,
-                difference_type rhs) { return lhs += rhs; }
-
-        friend SymmetricSquareArrayIterator operator-(
-                const SymmetricSquareArrayIterator & lhs,
-                difference_type rhs) { return lhs -= rhs; }
-
-        friend bool operator==(const SymmetricSquareArrayIterator & lhs,
-                               const SymmetricSquareArrayIterator & rhs) {
-            return lhs.row == rhs.row && lhs.col == rhs.col;
-        }
-
-        friend bool operator!=(const SymmetricSquareArrayIterator & lhs,
-                               const SymmetricSquareArrayIterator & rhs) {
-            return !(lhs == rhs);
-        }
-
-        friend bool operator<(const SymmetricSquareArrayIterator & lhs,
-                              const SymmetricSquareArrayIterator & rhs) {
-            return lhs.to_linear() < rhs.to_linear();
-        }
-
-        friend bool operator<=(const SymmetricSquareArrayIterator & lhs,
-                              const SymmetricSquareArrayIterator & rhs) {
-            return lhs.to_linear() <= rhs.to_linear();
-        }
-
-        friend bool operator>(const SymmetricSquareArrayIterator & lhs,
-                              const SymmetricSquareArrayIterator & rhs) {
-            return !(lhs.to_linear() <= rhs.to_linear());
-        }
-
-        friend bool operator>=(const SymmetricSquareArrayIterator & lhs,
-                              const SymmetricSquareArrayIterator & rhs) {
-            return !(lhs.to_linear() < rhs.to_linear());
-        }
-    };
-
-public:
-    using Iterator = SymmetricSquareArrayIterator<false>;
-    using ConstIterator = SymmetricSquareArrayIterator<true>;
-
-    Iterator begin() { return { this, 0, 0 }; }
-    Iterator end()   { return { this, rank, 0 }; }
-
-    ConstIterator begin() const { return { this, 0, 0 }; }
-    ConstIterator end()   const { return { this, rank, 0 }; }
-
-    ConstIterator cbegin() const { return { this, 0, 0 }; }
-    ConstIterator cend()   const { return { this, rank, 0 }; }
-
+    ImplementationHolder(const ImplementationHolder &) = default;
+    ImplementationHolder(ImplementationHolder &&) = default;
 };
 
 }
 
+template <typename ValueType, typename Allocator = std::allocator<ValueType>>
+class SymmetricSquareArray {
+    using ImplementationHolderType =
+        ssa::ImplementationHolder<ValueType, Allocator>;
+    using ImplementationType =
+        typename ImplementationHolderType::ImplementationType;
+
+    std::shared_ptr<ImplementationHolderType> holder;
+
+    void ensure_unique() {
+        if (holder.unique()) return;
+        holder = std::allocate_shared<ImplementationHolderType>(
+            holder->implementation.allocator,
+            *holder);
+    }
+
+    void enable_sharing() {
+        ensure_unique();
+        holder->is_sharable = true;
+    }
+
+    void disable_sharing() {
+        ensure_unique();
+        holder->is_sharable = false;
+    }
+
+public:
+    SymmetricSquareArray(Allocator allocator = Allocator())
+        : holder(
+            std::allocate_shared<ImplementationHolderType>(
+                allocator, allocator)) { }
+
+    SymmetricSquareArray(size_t rank, Allocator allocator = Allocator())
+        : holder(
+            std::allocate_shared<ImplementationHolderType>(
+                allocator, rank, allocator)) { }
+
+    SymmetricSquareArray(const SymmetricSquareArray & o)
+        : holder(
+            o.holder->is_sharable
+            ? o.holder
+            : std::allocate_shared<ImplementationHolderType>(
+                o.holder->implementation.allocator,
+                *o.holder)) { }
+
+    SymmetricSquareArray(SymmetricSquareArray &&) = default;
+
+    void insert(size_t row, size_t col,
+                const ValueType & val,
+                const ValueType & nil = ValueType()) {
+        enable_sharing();
+        holder->implementation.insert(row, col, val, nil);
+    }
+
+    void erase(size_t row, size_t col) {
+        enable_sharing();
+        holder->implementation.erase(row, col);
+    }
+
+    size_t get_rank() const { return holder->implementation.get_rank(); }
+
+    SymmetricSquareArray & operator=(SymmetricSquareArray o) {
+        swap(*this, o);
+        return *this;
+    }
+
+    ValueType & operator()(size_t row, size_t col) {
+        disable_sharing();
+        return holder->implementation(row, col);
+    }
+
+    const ValueType & operator()(size_t row, size_t col) const {
+        return holder->implementation(row, col);
+    }
+
+    static void swap(SymmetricSquareArray & lhs, SymmetricSquareArray & rhs) {
+        std::swap(lhs.holder, rhs.holder);
+    }
+
+    using Iterator      = typename ImplementationType::Iterator;
+    using ConstIterator = typename ImplementationType::ConstIterator;
+
+    Iterator begin() {
+        disable_sharing();
+        return holder->implementation.begin();
+    }
+
+    Iterator end() {
+        disable_sharing();
+        return holder->implementation.end();
+    }
+
+    ConstIterator begin() const { return holder->implementation.begin(); }
+    ConstIterator end()   const { return holder->implementation.end(); }
+
+    ConstIterator cbegin() const { return holder->implementation.cbegin(); }
+    ConstIterator cend()   const { return holder->implementation.cend(); }
+};
+
+}
